50547e404a80ea79ca6697e6900e3d1434654767
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-14 13:05:41 +0000

    feat: generate tab navbar component files

---
+++ src/app/shared/components/tabs/tab-navbar/tab-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './tab-navbar.component.html',
+  styleUrl: './tab-navbar.component.scss'
+})
+export class TabNavbarComponent {
+
+}
