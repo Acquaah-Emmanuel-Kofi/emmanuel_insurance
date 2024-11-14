2c725221a0222560ae117b80f7eb3e18e08e3729
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-14 12:18:23 +0000

    feat: render navbar in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, HeaderComponent],
+  imports: [RouterOutlet, MatSlideToggleModule, HeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
