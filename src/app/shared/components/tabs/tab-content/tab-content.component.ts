96d67c7171f614934f5f9724f23305737a455b10
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-14 15:09:17 +0000

    feat: implement tab content page

--- src/app/shared/components/tabs/tab-content/tab-content.component.ts
+++ src/app/shared/components/tabs/tab-content/tab-content.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-tab-content',
   standalone: true,
   imports: [],
   templateUrl: './tab-content.component.html',
-  styleUrl: './tab-content.component.scss'
+  styleUrl: './tab-content.component.scss',
 })
 export class TabContentComponent {
-
+  activeTab = input<number>(0);
 }
