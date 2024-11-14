350fe18014816a72cec7fe4348adabfb1f914d8d
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-14 15:10:41 +0000

    feat: render tab group in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { TabGroupComponent } from './shared/components/tabs/tab-group/tab-group.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, HeaderComponent],
+  imports: [RouterOutlet, MatSlideToggleModule, HeaderComponent, TabGroupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
