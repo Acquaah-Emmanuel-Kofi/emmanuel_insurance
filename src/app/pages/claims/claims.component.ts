6c1e1924f7634899c397dc737a301479eae27475
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-15 13:46:32 +0000

    feat: render header component on claims page

--- src/app/pages/claims/claims.component.ts
+++ src/app/pages/claims/claims.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ClaimsHeaderComponent } from './components/claims-header/claims-header.component';
 
 @Component({
   selector: 'app-claims',
   standalone: true,
-  imports: [],
+  imports: [ClaimsHeaderComponent],
   templateUrl: './claims.component.html',
   styleUrl: './claims.component.scss'
 })
