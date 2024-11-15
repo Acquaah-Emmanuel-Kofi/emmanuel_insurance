a301010090dd1448d02791648ec48559b2ced64b
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-15 14:51:09 +0000

    feat: implement searchbar component

--- src/app/shared/components/searchbar/searchbar.component.ts
+++ src/app/shared/components/searchbar/searchbar.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-searchbar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './searchbar.component.html',
-  styleUrl: './searchbar.component.scss'
+  styleUrl: './searchbar.component.scss',
 })
 export class SearchbarComponent {
+  searchTermValue = output<string>();
+  placeholder = input<string>('Search');
+  searchTerm: string = '';
 
+  handleSearchFilter(event: string) {
+    this.searchTermValue.emit(event);
+  }
 }
