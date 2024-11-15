08daccd4a3cd0bc7d2132a666503e0ad910d5abf
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-15 12:15:06 +0000

    refactor: remove all tab component and wherever used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { TabGroupComponent } from './shared/components/tabs/tab-group/tab-group.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, HeaderComponent, TabGroupComponent, NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'emmanuel_insurance';
