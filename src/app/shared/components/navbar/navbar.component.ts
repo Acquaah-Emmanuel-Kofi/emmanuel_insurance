50902dc42994c2e6c9b3f197a2e1580db1e5d5b7
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-15 12:04:38 +0000

    feat: structure navbar with navlinks

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 type Navlinks = {
   label: string;
   iconSrc: string;
+  routerLink: string;
 }
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
@@ -17,18 +19,22 @@ export class NavbarComponent {
     {
       label: 'Home',
       iconSrc: 'assets/icons/home.svg',
+      routerLink: '',
     },
     {
       label: 'Claims',
       iconSrc: 'assets/icons/claims.svg',
+      routerLink: 'claims',
     },
     {
       label: 'Quote History',
       iconSrc: 'assets/icons/quote_history.svg',
+      routerLink: 'history',
     },
     {
       label: 'Single Quote',
       iconSrc: 'assets/icons/single_quote.svg',
+      routerLink: 'single-quote',
     },
   ];
 }
