d5b8b751f84587e6a5229689ce55125efe67007d
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-15 14:51:43 +0000

    feat: render searchbar component on claims page

--- src/app/pages/claims/claims.component.ts
+++ src/app/pages/claims/claims.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ClaimsHeaderComponent } from './components/claims-header/claims-header.component';
+import { SearchbarComponent } from '../../shared/components/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-claims',
   standalone: true,
-  imports: [ClaimsHeaderComponent],
+  imports: [ClaimsHeaderComponent, SearchbarComponent],
   templateUrl: './claims.component.html',
   styleUrl: './claims.component.scss'
 })
