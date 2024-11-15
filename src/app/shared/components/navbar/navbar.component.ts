9fe70cfcf131b6abb19052e1b40c50f03ca30f00
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-15 12:00:44 +0000

    feat: define navlinks

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 
+type Navlinks = {
+  label: string;
+  iconSrc: string;
+}
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
+  navlinks: Navlinks[] = [
+    {
+      label: 'Home',
+      iconSrc: 'assets/icons/home.svg',
+    },
+    {
+      label: 'Claims',
+      iconSrc: 'assets/icons/claims.svg',
+    },
+    {
+      label: 'Quote History',
+      iconSrc: 'assets/icons/quote_history.svg',
+    },
+    {
+      label: 'Single Quote',
+      iconSrc: 'assets/icons/single_quote.svg',
+    },
+  ];
 }
