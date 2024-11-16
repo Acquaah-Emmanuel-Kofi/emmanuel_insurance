5686a7bcacf7e5bd667e4f011344d5306f4a6f3d
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-16 20:59:46 +0000

    feat: generate claim summary header component

---
+++ src/app/pages/claims/components/claim-summary/components/claim-summary-header/claim-summary-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-claim-summary-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './claim-summary-header.component.html',
+  styleUrl: './claim-summary-header.component.scss'
+})
+export class ClaimSummaryHeaderComponent {
+
+}
