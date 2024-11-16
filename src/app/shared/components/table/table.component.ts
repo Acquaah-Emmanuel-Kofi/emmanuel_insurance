83589565ff2e559e5953ac13294f7926ac5da45f
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-16 10:36:20 +0000

    feat: structure table component

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   standalone: true,
   imports: [],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrl: './table.component.scss',
 })
 export class TableComponent {
-
+  data = input<any[]>([]);
+  columns = input<{ label: string; field: string }[]>([]);
+  itemsPerPage = input<number>(15)
+  paginatedData: any[] = [];
+  currentPage: number = 1;
 }
