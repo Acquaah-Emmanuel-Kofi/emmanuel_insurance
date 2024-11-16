1dc4b2ec61fc724dc3842a2a2bc1d6b86eb6a244
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-16 23:27:32 +0000

    feat: struncture claim summary page with breadcrumb and searchbar components

--- src/app/pages/claims/components/claim-summary/claim-summary.component.ts
+++ src/app/pages/claims/components/claim-summary/claim-summary.component.ts
@@ -1,12 +1,16 @@
 import { Component, input } from '@angular/core';
+import { BreadcrumbComponent } from '../../../../shared/components/breadcrumb/breadcrumb.component';
+import { SearchbarComponent } from '../../../../shared/components/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-claim-summary',
   standalone: true,
-  imports: [],
+  imports: [BreadcrumbComponent, SearchbarComponent],
   templateUrl: './claim-summary.component.html',
   styleUrl: './claim-summary.component.scss',
 })
 export class ClaimSummaryComponent {
   claimNumber = input.required<string>();
+  policyHolder = input.required<string>();
+  policyNumber = input.required<string>();
 }
