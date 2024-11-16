fd32f055862fb9c42084e58149f5d699d8032369
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-16 16:02:49 +0000

    feat: integrate pagination into table component

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,17 +1,32 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, effect, input, model, OnInit, signal } from '@angular/core';
 import { TableHeaderComponent } from './components/table-header/table-header.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [TableHeaderComponent],
+  imports: [TableHeaderComponent, PaginationComponent],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
 export class TableComponent {
   data = input<any[]>([]);
   columns = input<{ label: string; field: string }[]>([]);
-  itemsPerPage = input<number>(15)
-  paginatedData: any[] = [];
-  currentPage: number = 1; 
+  itemsPerPage = signal<number>(10);
+  currentPage = signal<number>(1);
+
+  paginatedData = computed(() => {
+    const startIndex = (this.currentPage() - 1) * this.itemsPerPage();
+    const endIndex = startIndex + this.itemsPerPage();
+    return this.data().slice(startIndex, endIndex);
+  });
+
+  onPageChange(newPage: number): void {
+    this.currentPage.set(newPage);
+  }
+
+  onItemsPerPageChange(newItemsPerPage: number): void {
+    this.itemsPerPage.set(newItemsPerPage);
+    this.currentPage.set(1); 
+  }
 }
