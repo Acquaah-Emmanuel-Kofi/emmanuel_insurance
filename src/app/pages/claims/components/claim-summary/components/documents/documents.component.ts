f15fcc1d3206ddc7208a1bef5c07cb742964e4f8
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-17 23:54:01 +0000

    feat: integrate tree on document page with dummy data

--- src/app/pages/claims/components/claim-summary/components/documents/documents.component.ts
+++ src/app/pages/claims/components/claim-summary/components/documents/documents.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CustomTreeComponent } from '../../../../../../shared/components/custom-tree/custom-tree.component';
 
 @Component({
   selector: 'app-documents',
   standalone: true,
-  imports: [],
+  imports: [CustomTreeComponent],
   templateUrl: './documents.component.html',
-  styleUrl: './documents.component.scss'
+  styleUrl: './documents.component.scss',
 })
 export class DocumentsComponent {
-
+  treeData = [
+    {
+      label: 'CL1167845',
+      expanded: false,
+      children: [
+        { label: 'Attached Documents' },
+        { label: 'Emails Received' },
+        { label: 'Miscellaneous' },
+      ],
+    },
+  ];
 }
