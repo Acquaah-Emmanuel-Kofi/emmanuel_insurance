28ea58cda0c9dd01df7a75ccd0549072af6c46ff
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-17 13:27:14 +0000

    feat: implement form with dummy data

--- src/app/pages/claims/components/claim-summary/components/dairy-form/dairy-form.component.ts
+++ src/app/pages/claims/components/claim-summary/components/dairy-form/dairy-form.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { InputFieldComponent } from '../../../../../../shared/components/input-field/input-field.component';
+import { DialogRef } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-dairy-form',
   standalone: true,
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    InputFieldComponent,
+  ],
   templateUrl: './dairy-form.component.html',
-  styleUrl: './dairy-form.component.scss'
+  styleUrl: './dairy-form.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DairyFormComponent {
+  dialogRef = inject<DialogRef<string>>(DialogRef<string>);
 
+  typeOptions: { label: string; value: string }[] = [
+    { label: 'Type 1', value: 'type1' },
+    { label: 'Type 2', value: 'type2' },
+  ];
+
+  priorityOptions: { label: string; value: string }[] = [
+    { label: 'High', value: 'high' },
+    { label: 'Medium', value: 'medium' },
+    { label: 'Low', value: 'low' },
+  ];
+
+  form: FormGroup;
+  private _formBuilder = inject(FormBuilder);
+
+  constructor() {
+    this.form = this._formBuilder.group({
+      type: ['', Validators.required],
+      title: ['', Validators.required],
+      priority: ['', Validators.required],
+      dueDate: ['', Validators.required],
+      description: [''],
+      confidential: [false],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Form Submitted: ', this.form.value);
+    }
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
 }
