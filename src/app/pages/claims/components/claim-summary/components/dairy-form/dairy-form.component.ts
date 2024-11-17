49d668a87b5ecfd8285fdf978dfd4b8c41295414
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-17 10:51:18 +0000

    feat: generate dairy form component

---
+++ src/app/pages/claims/components/claim-summary/components/dairy-form/dairy-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dairy-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './dairy-form.component.html',
+  styleUrl: './dairy-form.component.scss'
+})
+export class DairyFormComponent {
+
+}
