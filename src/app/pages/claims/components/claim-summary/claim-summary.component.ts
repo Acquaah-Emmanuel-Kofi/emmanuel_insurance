42f5cbc9858e7ac55589d0077bd32b8612501496
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-17 01:12:24 +0000

    feat: integrate claim tabs component on page

--- src/app/pages/claims/components/claim-summary/claim-summary.component.ts
+++ src/app/pages/claims/components/claim-summary/claim-summary.component.ts
@@ -1,11 +1,13 @@
 import { Component, input } from '@angular/core';
 import { BreadcrumbComponent } from '../../../../shared/components/breadcrumb/breadcrumb.component';
 import { SearchbarComponent } from '../../../../shared/components/searchbar/searchbar.component';
+import { ClaimTabsComponent } from './components/claim-tabs/claim-tabs.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-claim-summary',
   standalone: true,
-  imports: [BreadcrumbComponent, SearchbarComponent],
+  imports: [BreadcrumbComponent, SearchbarComponent, ClaimTabsComponent, RouterOutlet],
   templateUrl: './claim-summary.component.html',
   styleUrl: './claim-summary.component.scss',
 })
