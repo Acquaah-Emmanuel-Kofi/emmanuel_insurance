a2fc601cbf0e633176293110abad98ce395d941d
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-17 17:19:03 +0000

    feat: design stat card

--- src/app/pages/claims/components/claim-summary/components/financial/components/financial-stat-card/financial-stat-card.component.ts
+++ src/app/pages/claims/components/claim-summary/components/financial/components/financial-stat-card/financial-stat-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ILabelValue } from '../../../../../../../../shared/interfaces/constants.interface';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-financial-stat-card',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './financial-stat-card.component.html',
-  styleUrl: './financial-stat-card.component.scss'
+  styleUrl: './financial-stat-card.component.scss',
 })
 export class FinancialStatCardComponent {
-
-}
+  title = input.required<string>();
+  price = input.required<number>();
+  subPrices = input<ILabelValue[]>([]);
+} 
