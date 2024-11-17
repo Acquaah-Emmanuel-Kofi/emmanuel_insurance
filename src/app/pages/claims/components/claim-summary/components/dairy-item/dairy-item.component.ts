ee0bba94da0a30938dd7a7fd581b7e0089729801
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-17 10:30:12 +0000

    feat: integrate empty data component on page

--- src/app/pages/claims/components/claim-summary/components/dairy-item/dairy-item.component.ts
+++ src/app/pages/claims/components/claim-summary/components/dairy-item/dairy-item.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { EmptyDataComponent } from '../../../../../../shared/components/empty-data/empty-data.component';
 
 @Component({
   selector: 'app-dairy-item',
   standalone: true,
-  imports: [],
+  imports: [EmptyDataComponent],
   templateUrl: './dairy-item.component.html',
-  styleUrl: './dairy-item.component.scss'
+  styleUrl: './dairy-item.component.scss',
 })
 export class DairyItemComponent {
-
+  subTitleText = 'Click "Add New Diary Item" to create one';
 }
