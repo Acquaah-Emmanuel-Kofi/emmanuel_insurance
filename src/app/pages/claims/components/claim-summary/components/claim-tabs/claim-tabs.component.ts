682c1828980bfd79cdcfc98c0f7448b18d1cb045
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-17 00:06:35 +0000

    feat: generate claim tabs component

---
+++ src/app/pages/claims/components/claim-summary/components/claim-tabs/claim-tabs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-claim-tabs',
+  standalone: true,
+  imports: [],
+  templateUrl: './claim-tabs.component.html',
+  styleUrl: './claim-tabs.component.scss'
+})
+export class ClaimTabsComponent {
+
+}
