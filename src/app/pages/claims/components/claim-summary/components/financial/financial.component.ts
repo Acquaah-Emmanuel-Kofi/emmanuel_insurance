5a43a04f97d3340c02fe1b09dd84138abb987aeb
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-17 17:19:37 +0000

    feat: structure financial page with stat cards

--- src/app/pages/claims/components/claim-summary/components/financial/financial.component.ts
+++ src/app/pages/claims/components/claim-summary/components/financial/financial.component.ts
@@ -1,12 +1,69 @@
 import { Component } from '@angular/core';
+import { FinancialStatCardComponent } from './components/financial-stat-card/financial-stat-card.component';
+import { ILabelValue } from '../../../../../../shared/interfaces/constants.interface';
+
+type StatCards = {
+  title: string;
+  price: number;
+  subPrices: ILabelValue[];
+}
 
 @Component({
   selector: 'app-financial',
   standalone: true,
-  imports: [],
+  imports: [FinancialStatCardComponent],
   templateUrl: './financial.component.html',
-  styleUrl: './financial.component.scss'
+  styleUrl: './financial.component.scss',
 })
 export class FinancialComponent {
-
+  financialStatCards: StatCards[] = [
+    {
+      title: 'Claimed',
+      price: 5000,
+      subPrices: [
+        {
+          label: 'Initial Claimed',
+          value: 5999,
+        },
+      ],
+    },
+    {
+      title: 'Excess',
+      price: 250,
+      subPrices: [
+        {
+          label: 'Initial Excess',
+          value: 250,
+        },
+        {
+          label: 'Excess Recovered',
+          value: 0,
+        },
+      ],
+    },
+    {
+      title: 'Paid',
+      price: 0,
+      subPrices: [
+        {
+          label: 'Recovered',
+          value: 0,
+        },
+        {
+          label: 'Salvaged',
+          value: 0,
+        },
+      ],
+    },
+    {
+      title: 'Balance',
+      price: 4850,
+      subPrices: [
+        {
+          label: 'Cost of Claim',
+          value: 4850,
+        },
+      ],
+    },
+  ];
 }
