b0387401876cd0c021caa6d69e7a4bf6d89e1193
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-17 13:25:50 +0000

    feat: use mat dailog to pop up dairy form component

--- src/app/pages/claims/components/claim-summary/components/dairy-item/dairy-item.component.ts
+++ src/app/pages/claims/components/claim-summary/components/dairy-item/dairy-item.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { EmptyDataComponent } from '../../../../../../shared/components/empty-data/empty-data.component';
+import {
+  Dialog,
+  DialogModule,
+} from '@angular/cdk/dialog';
+import { DairyFormComponent } from '../dairy-form/dairy-form.component';
 
 @Component({
   selector: 'app-dairy-item',
   standalone: true,
-  imports: [EmptyDataComponent],
+  imports: [EmptyDataComponent, DialogModule],
   templateUrl: './dairy-item.component.html',
   styleUrl: './dairy-item.component.scss',
 })
 export class DairyItemComponent {
   subTitleText = 'Click "Add New Diary Item" to create one';
+
+  readonly dialog = inject(Dialog);
+
+  openDialog() {
+    this.dialog.open<string>(DairyFormComponent);
+  }
 }
