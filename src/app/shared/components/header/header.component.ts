354a584960a2f1741a3612c9b51c4361879c01bc
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-18 01:03:10 +0000

    feat: get name initials on header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { getFirstTwoInitials } from '../../helpers/functions.helper';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  nameInitials(name: string) {
+    return getFirstTwoInitials(name);
+  }
 }
