daaba2657f64492b45c7e383bac4369fc27268cd
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-18 00:54:44 +0000

    feat: add components to routes

--- src/app/shared/components/layout/layout.routes.ts
+++ src/app/shared/components/layout/layout.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { ClaimsComponent } from '../../../pages/claims/claims.component';
+import { QuoteHistoryComponent } from '../../../pages/quote-history/quote-history.component';
+import { SingleQuoteComponent } from '../../../pages/single-quote/single-quote.component';
+import { HomeComponent } from '../../../pages/home/home.component';
 
 export const layoutRoutes: Routes = [
   {
@@ -8,11 +11,23 @@ export const layoutRoutes: Routes = [
     pathMatch: 'full',
   },
   {
+    path: '',
+    component: HomeComponent
+  },
+  {
     path: 'claims',
     component: ClaimsComponent,
     loadChildren: () =>
       import('../../../pages/claims/claims.routes').then(
         (routes) => routes.claimsRoutes
       ),
+  },
+  {
+    path: 'quote-history',
+    component: QuoteHistoryComponent,
+  },
+  {
+    path: 'single-quote',
+    component: SingleQuoteComponent,
   },
 ];
