b9ee4aa3545ee7152b8b3c890bd45812424fd08f
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2024-11-18 00:56:57 +0000

    feat: generate risk at dol component

---
+++ src/app/pages/claims/components/claim-summary/components/risk-at-dol/risk-at-dol.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-risk-at-dol',
+  standalone: true,
+  imports: [],
+  templateUrl: './risk-at-dol.component.html',
+  styleUrl: './risk-at-dol.component.scss'
+})
+export class RiskAtDolComponent {
+
+}
